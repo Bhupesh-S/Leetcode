2071fb801e70d61c518d3154d8b1fa4492b41fc6
BHUPESH SANTHA KUMAR <bhupeshsanthakumar.23cse@kongu.edu>
2026-09-23 18:09:14 +0530

    Time: 1 ms (99.94%), Space: 94.7 MB (15.79%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit=0;
+        int minBuy=prices[0];
+        for(int p:prices){
+            maxProfit=Math.max(maxProfit,p-minBuy);
+            minBuy=Math.min(minBuy,p);
+        }
+        return maxProfit;
+    }
+}
